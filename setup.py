a3aa16b5991e5aa512f11671265a77e1fb29170c
code-ape <ferris@ferrisellis.com>
2014-03-10 01:54:59 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(name='pyspec',
+      version='0.1',
+      description='Python DBB platform',
+      author='codeape',
+      packages=['pyspec']
+     )
+
